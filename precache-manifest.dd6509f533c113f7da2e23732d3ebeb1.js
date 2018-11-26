7fb73502c73f9def0693076b6f518994e89d9082
Aliaksei.Kahan <aliaksei.kagan@gmail.com>
2018-11-26 17:56:07 +0300

    Updates

---
+++ precache-manifest.dd6509f533c113f7da2e23732d3ebeb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb52996c0cbd2cc8233b33b7cc2f1850",
+    "url": "/touchside/static/media/snow-falling.bb52996c.jpg"
+  },
+  {
+    "revision": "7ade0b85ae1288ac631afdc66381a1b3",
+    "url": "/touchside/static/media/mist.7ade0b85.jpg"
+  },
+  {
+    "revision": "b631b05b2a29dee83e1bd7345c5109dc",
+    "url": "/touchside/static/media/carousel-3.b631b05b.jpg"
+  },
+  {
+    "revision": "0f793975aeb052049d6260f2109d0da0",
+    "url": "/touchside/static/media/carousel-2.0f793975.jpg"
+  },
+  {
+    "revision": "55b8410f8555a22a77a2760da5434381",
+    "url": "/touchside/static/media/carousel-1.55b8410f.jpg"
+  },
+  {
+    "revision": "ecac97e2db8ae4bd42e7",
+    "url": "/touchside/static/js/runtime~main.ecac97e2.js"
+  },
+  {
+    "revision": "84139592d14a5563d179",
+    "url": "/touchside/static/js/main.84139592.chunk.js"
+  },
+  {
+    "revision": "68a0fd753de087810048",
+    "url": "/touchside/static/js/1.68a0fd75.chunk.js"
+  },
+  {
+    "revision": "84139592d14a5563d179",
+    "url": "/touchside/static/css/main.6fa51468.chunk.css"
+  },
+  {
+    "revision": "4570070f6f7d3e05c3488b0c56dd248f",
+    "url": "/touchside/index.html"
+  }
+];
