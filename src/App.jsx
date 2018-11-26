95371dc70d3a3c1c3e2654d1a4b780c7fe050d7a
Aliaksei.Kahan <aliaksei.kagan@gmail.com>
2018-11-26 17:13:31 +0300

    Added redirection to Home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 import MainView from './common/containers/main-view/MainView';
 import Contact from './sections/contact/Contact';
@@ -18,6 +23,7 @@ function App() {
         <Switch>
           <Route exact path={MAIN_ROUTING.HOME} component={Home} />
           <Route exact path={MAIN_ROUTING.CONTACT} component={Contact} />
+          <Redirect to={MAIN_ROUTING.HOME} />
         </Switch>
       </MainView>
     </BrowserRouter>
